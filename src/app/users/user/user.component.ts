db0ff1e77387c57c81a6a83b61fe4c441752af19
puzzelena <puzic.elena1@gmail.com>
2021-08-18 17:31:40 +0300

    133. Fetching Route Parameters

--- src/app/users/user/user.component.ts
+++ src/app/users/user/user.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-user',
@@ -7,10 +8,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UserComponent implements OnInit {
   user: {id: number, name: string};
-
-  constructor() { }
+// we need to inject the active route
+// ActivatedRoute is special method of angular/route
+  constructor(private route: ActivatedRoute) { }
 
   ngOnInit() {
+    // here we will get our user
+    this.user = {
+      id: this.route.snapshot.params['id'],
+      // it takes id from the app.module.ts where is declared dynamic id
+      name: this.route.snapshot.params['name']
+    }
   }
 
 }

4de6f9cb5fb4834443f7eb80ca7e688411bcdee7
puzzelena <puzic.elena1@gmail.com>
2021-08-19 08:50:36 +0300

    135. An Important Note about Route Observables

--- src/app/users/user/user.component.ts
+++ src/app/users/user/user.component.ts
@@ -1,13 +1,15 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { ActivatedRoute, Params } from '@angular/router';
+import { Subscription } from 'rxjs/Subscription';
 
 @Component({
   selector: 'app-user',
   templateUrl: './user.component.html',
   styleUrls: ['./user.component.css']
 })
-export class UserComponent implements OnInit {
+export class UserComponent implements OnInit, OnDestroy {
   user: {id: number, name: string};
+  paramsSubscription: Subscription
 // we need to inject the active route
 // ActivatedRoute is special method of angular/route
   constructor(private route: ActivatedRoute) { }
@@ -25,7 +27,7 @@ export class UserComponent implements OnInit {
     // work with asynchronous tasks
     // observable may you to subscribe to an event which might
     // happen in the future to then execute some code
-    this.route.params
+    this.paramsSubscription = this.route.params
     .subscribe(
       // here we will get an updated parameters of type Params
       // Params will be always an object that we pass which holds the parameters defined in the routes
@@ -40,6 +42,12 @@ export class UserComponent implements OnInit {
     // we call the subscribe method
     // the first parameter should be a function
 
+    /*ngOnDestroy() {
+      this.paramsSubscription.unsubscribe()
+    }
+    */
+
   }
 
 }
+

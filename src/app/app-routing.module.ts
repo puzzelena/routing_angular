561ff5aeb1a39cc9dde04c39d8c983dae4334e97
puzzelena <puzic.elena1@gmail.com>
2021-08-19 12:46:29 +0300

    150. Passing Static Data to a Route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,7 @@
-import { NgModule } from "@angular/core";
+import { ErrorHandler, NgModule } from "@angular/core";
 import { Routes, RouterModule } from "@angular/router";
 import { AuthGuard } from "./auth-guard.service";
+import { ErrorPageComponent } from "./error-page/error-page.component";
 
 import { HomeComponent } from "./home/home.component";
 import { PageNotFoundComponent } from "./page-not-found/page-not-found.component";
@@ -16,46 +17,52 @@ import { UsersComponent } from "./users/users.component";
 
 const appRoutes: Routes = [
     // this structure always need a path that can be added at the end of the domain name
-    { 
-        path: '', 
+    {
+        path: '',
         component: HomeComponent 
     }, // it will look like localhost:4200/users
     // component shows which component should be loaded
     // HomeComponent as the starter page
-    { 
-        path: 'users', 
-        component: UsersComponent, 
+    {
+        path: 'users',
+        component: UsersComponent,
     children: [
-      { 
-        path: ':id/:name', 
-        component: UserComponent 
+      {
+        path: ':id/:name',
+        component: UserComponent
       },
     ]
     },
     // : -> shows that it is dynamic
-    { 
-        path: 'servers', 
-        //canActivate:[AuthGuard], 
+    {
+        path: 'servers',
+        //canActivate:[AuthGuard],
         canActivateChild: [AuthGuard],
         component: ServersComponent,
     children: [
-      { 
-        path: '/:id', 
-        component: ServerComponent 
+      {
+        path: '/:id',
+        component: ServerComponent
       },
-      { 
-        path: '/:id/edit', 
+      {
+        path: '/:id/edit',
         component: EditServerComponent,
         canDeactivate: [CanDeactivateGuard]
       },
       // component loads specified component and its content
     ] },
-    { 
+   /* { 
         path: 'not-found', 
         component: PageNotFoundComponent
     },
+    */
     { 
-        path: '**', 
+        path: 'not-found',
+        component: ErrorPageComponent,
+        data: {message: 'Page not found!'}
+    },
+    {
+        path: '**',
         redirectTo: '/not-found'
     }
     // redirectTo redirects to another path
@@ -65,7 +72,7 @@ const appRoutes: Routes = [
     // if we write something wrong in the path it takes ** route and redirect it to
     // not-found path with the component created PageNotFoundComponent
   ];
-  
+
 @NgModule({
     imports: [
         RouterModule.forRoot(appRoutes)
